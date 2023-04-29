5a67451b11a33c7c8b2c1905bae16a9b2c452542
Peter <peterng.918@gmail.com>
2023-04-28 20:36:08 -0400

    added reviews for each movie

--- pages/movies/[movie].js
+++ pages/movies/[movie].js
@@ -4,19 +4,30 @@ export default function SingleMovie({}) {
   const router = useRouter();
   const { movie } = router.query;
   const [single, setSingle] = useState({});
+  const [reviews, setReviews] = useState({});
   useEffect(() => {
     async function ret() {
       const data = await fetch(`/api/movies/${movie}`);
       console.log(data);
       const one = await data.json();
       console.log(one);
+      const reviewData = await fetch(`/api/reviews/${movie}`);
+      console.log(movie);
+      const reviewJson = await reviewData.json();
+      console.log("reviewwwwwww", reviewJson);
       setSingle(one);
+      setReviews(reviewJson);
     }
     ret();
   }, []);
   return (
     <>
       <p>{movie}</p>
+      <ul>
+        {reviews.map((review) => {
+          return <li key={review._id}>{review.review}</li>;
+        })}
+      </ul>
       <img src={single.poster} />
     </>
   );
