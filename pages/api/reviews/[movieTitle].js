419c27aec21a63ab86288be4d6f9e1a1775780a8
Peter <peterng.918@gmail.com>
2023-05-01 10:14:26 -0400

    added delete icon for reviews

--- pages/api/reviews/[movieTitle].js
+++ pages/api/reviews/[movieTitle].js
@@ -35,6 +35,7 @@ export default async function handler(req, res) {
     } catch (error) {
       console.log(error);
     }
+  } else if (req.method == "DELETE") {
   } else {
     res.status(404).json({ error: "wrong action" });
   }
