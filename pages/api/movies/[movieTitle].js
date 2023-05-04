3c9579717f2d6efef86aadae0027c1957a9a48e8
Peter <peterng.918@gmail.com>
2023-05-04 11:20:06 -0400

    protected one [movie] api route if not logged in

--- pages/api/movies/[movieTitle].js
+++ pages/api/movies/[movieTitle].js
@@ -1,12 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getServerSession } from "next-auth";
 import { moviesCollections } from "../../../db/index";
+import { authOptions } from "../auth/[...nextauth]";
 export default async function handler(req, res) {
-  try {
-    const { movieTitle } = req.query;
-    const query = { title: movieTitle };
+  const session = await getServerSession(req, res, authOptions);
+  if (session) {
+    try {
+      const { movieTitle } = req.query;
+      const query = { title: movieTitle };
 
-    let results = await moviesCollections.findOne(query);
+      let results = await moviesCollections.findOne(query);
 
-    res.status(200).json(results);
-  } catch (error) {}
+      res.status(200).json(results);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(401).json("not signed in");
+  }
+  res.end();
 }
