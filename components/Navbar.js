fcbeac446832b77b6233416853ec78d43d6ad746
Peter <peterng.918@gmail.com>
2023-05-05 10:11:36 -0400

    basic navbar completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
 export default function Navbar() {
   return (
-    <>
-      <p>NavBar</p>
-    </>
+    <div className="flex bg-neutral-300">
+      <Link className="navButtons" href={"/"}>
+        <p className="text-decoration-line : underline">HomeButton</p>
+      </Link>
+      <Link className="navButtons" href={"/movies"}>
+        <p className="text-decoration-line : underline">Movies</p>
+      </Link>
+    </div>
   );
 }
