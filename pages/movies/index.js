0b229fbbad94f890723955f5a019e875ae7901a1
Peter <peterng.918@gmail.com>
2023-07-10 00:29:59 -0400

    updated movie list view

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -42,16 +42,11 @@ export default function MovieHome() {
   if (session) {
     return (
       <div>
-        {/* <button
-          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-2"
-          onClick={sortHandle}
-        >
-          sort
-        </button> */}
         <div className="bg-slate-400">
           {/* <label className="bg-slate-400 mx-4">Sort</label> */}
           <select
-            className="ml-4 rounded-sm"
+            // className="ml-4 rounded-sm"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-36 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             onChange={(e) => {
               // console.log(e.currentTarget.value);
               const sort = e.currentTarget.value;
@@ -77,35 +72,83 @@ export default function MovieHome() {
             <option value={-1}>Z...A</option>
           </select>
         </div>
-        {movies.map((movie) => {
-          return (
-            <div
-              className="w-4/5 flex m-4 p-4 bg-slate-200 rounded-md"
-              key={movie.title}
-            >
-              <Image src={movie.poster} width={"100"} height={"100"} />
-              <div className="m-2 p-2">
-                <Link href={`/movies/${movie.title}`}>
-                  <p className="underline">{movie.title}</p>
-                </Link>
-                <p>{`Release date : ${movie.releaseDate}`}</p>
-                <div>
+        <div className="md:flex flex-col items-center">
+          {/* <button
+          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-2"
+          onClick={sortHandle}
+        >
+        sort
+      </button> */}
+
+          {movies.map((movie) => {
+            return (
+              <Link
+                key={movie._id}
+                href={`/movies/${movie.title}`}
+                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:min-w-reviewBoxWidthMd hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+              >
+                <Image
+                  class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                  src={movie.poster}
+                  alt=""
+                  width={400}
+                  height={400}
+                />
+
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {movie.title}
+                  </h5>
                   <ul>
                     {movie.genres &&
                       movie.genres.map((genre) => {
                         return (
-                          <li key={genre} className="text-xs list-disc">
+                          <li
+                            key={genre}
+                            className="list-disc font-normal text-gray-700 dark:text-gray-400"
+                          >
                             {`${genre}`}
                           </li>
                         );
                       })}
+                    {/* <li className="list-disc font-normal text-gray-700 dark:text-gray-400"></li> */}
                   </ul>
                 </div>
-              </div>
+              </Link>
+            );
+          })}
+          {/* {movies.map((movie) => {
+          return (
+            <div
+            className="w-4/5 flex m-4 p-4 bg-slate-200 rounded-md"
+            key={movie.title}
+            >
+            <Image src={movie.poster} width={"100"} height={"100"} />
+            <div className="m-2 p-2">
+            <Link href={`/movies/${movie.title}`}>
+            <p className="underline">{movie.title}</p>
+            </Link>
+            <p>{`Release date : ${movie.releaseDate}`}</p>
+            <div>
+            <ul>
+            {movie.genres &&
+              movie.genres.map((genre) => {
+                return (
+                  <li key={genre} className="text-xs list-disc">
+                  {`${genre}`}
+                  </li>
+                  );
+                })}
+                </ul>
+                </div>
+                </div>
             </div>
           );
-        })}
-        {session.user.role == "admin" && <button onClick={addTest}>Add</button>}
+        })} */}
+          {session.user.role == "admin" && (
+            <button onClick={addTest}>Add</button>
+          )}
+        </div>
       </div>
     );
   } else {
